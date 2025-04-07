dbe6dda4794a5eddc48ec231f9636f7725454e38
Irene Modestus <irenechitemi657@gmail.com>
2025-04-07 18:00:10 +0530

    Project completed

---
+++ back/src/seeds/user.seed.js
@@ -0,0 +1,29 @@
+import { config } from "dotenv";
+import { connectDB } from "../lib/db.js";
+import User from "../models/user.model.js";
+
+config();
+
+const seedUsers = [
+  // Female Users
+  {
+    email: "chica@gmai.com",
+    fullName: "Chix Cateh",
+    password: "chica@2603",
+    profilePic: "",
+  },
+];
+
+const seedDatabase = async () => {
+  try {
+    await connectDB();
+
+    await User.insertMany(seedUsers);
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+};
+
+// Call the function
+seedDatabase();
